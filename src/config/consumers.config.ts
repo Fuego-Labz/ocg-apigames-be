eec4b95854c124469fcd135c240bfe7afe6df104
cesarrsegovia <cesarr.segovia27@gmail.com>
2026-04-21 15:50:41 -0300

    add consumers

--- src/config/consumers.config.ts
+++ src/config/consumers.config.ts
@@ -4,24 +4,11 @@
 export const CONSUMER_PROVIDERS: Record<string, string[]> = {
   default: ['4', '7', '8', '12', '58', '64'], // PragmaticPlay, Fugaso, BGaming, Yggdrasil, TurboGames, PGSoft
   '777vault': [
-    '1',   // BetSoft
-    '3',   // TomHorn
-    '4',   // PragmaticPlay
-    '7',   // Fugaso
-    '8',   // BGaming
-    '12',  // Yggdrasil
-    '30',  // Ruby Play
-    '58',  // TurboGames
-    '64',  // PGSoft
-    '70',  // Rogue
-    '72',  // Ethereal
-    '76',  // AvatarUX
-    '81',  // Eurasian
-    '92',  // Blaze (no existe en DB todavía)
-    '93',  // Peter And Sons
-    '101', // InOut Games
-    '102', // 77 Gaming
-    '110', // Aviator LLC
+    '0',  // Lucky Streak (Live)
+    '3',  // TomHorn
+    '4',  // PragmaticPlay
+    '12', // Yggdrasil
+    '30', // Ruby Play
   ],
 };
 
